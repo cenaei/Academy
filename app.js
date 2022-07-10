fa630c5c6986b5b33b72b5c55b10e129c463607e
Sina Sangiji <sina.sangiji@gmail.com>
2022-07-10 20:02:29 +0430

    Added Preloader

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const tabs = document.querySelectorAll(".tabs");
 const tabMenu = document.querySelectorAll(".tab-menu");
+const splashScreen = document.querySelector(".splash-screen")
 
+window.addEventListener('DOMContentLoaded', (event) => {
+  setTimeout(() => {
+    splashScreen.classList.add("hidden")
+  }, 2000)
+});
 // console.log("tabs", tabs)
 // console.log("tabMenu", tabMenu)
 
